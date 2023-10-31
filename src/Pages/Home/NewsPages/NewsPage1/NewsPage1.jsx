e9874999e663852f0d5ac5d7784c229ff3d5779b
TabasshinAfrinMeghla <tabasshinafrinmeghla@gmail.com>
2023-10-31 17:57:17 +0600

    add all

---
+++ src/Pages/Home/NewsPages/NewsPage1/NewsPage1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsPage1 = () => {
+  return (
+    <div>
+      <h2>News coming soon</h2>
+    </div>
+  );
+};
+
+export default NewsPage1;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,7 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Category from "../Pages/Home/Category";
-import News_Layout from "../Pages/Home/NewsPages/NewsPage1/News_Layout";
+import NewsPage1 from "../Pages/Home/NewsPages/NewsPage1/NewsPage1";
+import News_Layout from "../Pages/Home/NewsPages/NewsPageLayaout/News_Layout";
+
 
 
 const router = createBrowserRouter([
@@ -28,8 +30,15 @@ const router = createBrowserRouter([
   }
   ,
   {
-    path: '/',
+    path: '/news',
     element: <News_Layout></News_Layout>,
+
+    children: [
+      {
+        path: ':id',
+        element: <NewsPage1></NewsPage1>,
+      }
+    ]
   }
 ])
 
