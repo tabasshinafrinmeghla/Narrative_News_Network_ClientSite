812e23f13f9c0a4dd1ba5e7239058578ec0d6527
TabasshinAfrinMeghla <tabasshinafrinmeghla@gmail.com>
2023-11-01 18:26:37 +0600

    update

--- src/Pages/Home/Category.jsx
+++ src/Pages/Home/Category.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import NewsCard from './NewsCard/Newscard';
 
 const Category = () => {
   const { id } = useParams();
@@ -7,6 +8,13 @@ const Category = () => {
   return (
     <div>
       <h3>this is Category :{categoryNews.length}</h3>
+
+      {
+        categoryNews.map(news => <NewsCard
+          key={news._id}
+          news={news}
+        ></NewsCard>)
+      }
     </div>
   );
 };
--- src/Pages/Home/NewsCard/Newscard.jsx
+++ src/Pages/Home/NewsCard/Newscard.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-const NewsCard = () => {
+const NewsCard = ({ news }) => {
+
+
+  const { _id, image_url, details, published_date, name, title, total_view, author, rating, number, badge, thumbnail_url } = news;
   return (
     <div>
+      <Card className="mb-4">
+        <Card.Header>Featured</Card.Header>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Img variant="top" src={image_url} />
+          <Card.Text>
+            {details.length < 300 ? <>{details} </> : <>{details.slice(0, 300)} ... <Link to={`/news/${_id}`}>Read moor</Link>   </>}
+          </Card.Text>
 
+        </Card.Body>
+        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      </Card>
     </div>
   );
 };
